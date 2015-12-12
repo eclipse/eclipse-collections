1e290f2fa26dd7ffa655ddf72f5bf7b939ae8122
Craig P. Motlin <craig.motlin@gs.com>
2015-12-12 15:33:56 -0500

    Format all Copyright headers.

--- unit-tests-java8/src/test/java/org/eclipse/collections/test/bimap/BiMapTestCase.java
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/bimap/BiMapTestCase.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2015 Goldman Sachs.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -6,7 +6,7 @@
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
  * and the Eclipse Distribution License is available at
  * http://www.eclipse.org/org/documents/edl-v10.php.
- *******************************************************************************/
+ */
 
 package org.eclipse.collections.test.bimap;
 
