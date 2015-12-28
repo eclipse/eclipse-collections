71c787c194606b06d7ffec1d7a8c21175219540c
Craig P. Motlin <craig.motlin@gs.com>
2015-12-28 09:51:39 -0500

    Make primitive-object maps more bag-like. Change the filtering and transformation methods to return bags since they contain duplicates and no meaningful order.

--- eclipse-collections-api/src/main/java/org/eclipse/collections/api/map/primitive/MutablePrimitiveObjectMap.java
+++ eclipse-collections-api/src/main/java/org/eclipse/collections/api/map/primitive/MutablePrimitiveObjectMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Goldman Sachs.
+ * Copyright (c) 2016 Goldman Sachs.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
@@ -11,6 +11,14 @@
 package org.eclipse.collections.api.map.primitive;
 
 import org.eclipse.collections.api.bag.MutableBag;
+import org.eclipse.collections.api.bag.primitive.MutableBooleanBag;
+import org.eclipse.collections.api.bag.primitive.MutableByteBag;
+import org.eclipse.collections.api.bag.primitive.MutableCharBag;
+import org.eclipse.collections.api.bag.primitive.MutableDoubleBag;
+import org.eclipse.collections.api.bag.primitive.MutableFloatBag;
+import org.eclipse.collections.api.bag.primitive.MutableIntBag;
+import org.eclipse.collections.api.bag.primitive.MutableLongBag;
+import org.eclipse.collections.api.bag.primitive.MutableShortBag;
 import org.eclipse.collections.api.block.function.Function;
 import org.eclipse.collections.api.block.function.Function0;
 import org.eclipse.collections.api.block.function.Function2;
@@ -25,15 +33,6 @@ import org.eclipse.collections.api.block.function.primitive.ShortFunction;
 import org.eclipse.collections.api.block.predicate.Predicate;
 import org.eclipse.collections.api.block.predicate.Predicate2;
 import org.eclipse.collections.api.block.procedure.Procedure2;
-import org.eclipse.collections.api.collection.MutableCollection;
-import org.eclipse.collections.api.collection.primitive.MutableBooleanCollection;
-import org.eclipse.collections.api.collection.primitive.MutableByteCollection;
-import org.eclipse.collections.api.collection.primitive.MutableCharCollection;
-import org.eclipse.collections.api.collection.primitive.MutableDoubleCollection;
-import org.eclipse.collections.api.collection.primitive.MutableFloatCollection;
-import org.eclipse.collections.api.collection.primitive.MutableIntCollection;
-import org.eclipse.collections.api.collection.primitive.MutableLongCollection;
-import org.eclipse.collections.api.collection.primitive.MutableShortCollection;
 import org.eclipse.collections.api.map.MutableMap;
 import org.eclipse.collections.api.multimap.bag.MutableBagMultimap;
 import org.eclipse.collections.api.ordered.OrderedIterable;
@@ -41,7 +40,7 @@ import org.eclipse.collections.api.partition.bag.PartitionMutableBag;
 import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.api.tuple.Pair;
 
-public interface MutablePrimitiveObjectMap<V>
+public interface MutablePrimitiveObjectMap<V> extends PrimitiveObjectMap<V>
 {
     void clear();
 
@@ -57,35 +56,37 @@ public interface MutablePrimitiveObjectMap<V>
 
     <VV> MutableBag<VV> collectIf(Predicate<? super V> predicate, Function<? super V, ? extends VV> function);
 
-    <VV> MutableCollection<VV> collect(Function<? super V, ? extends VV> function);
+    <VV> MutableBag<VV> collect(Function<? super V, ? extends VV> function);
 
-    MutableBooleanCollection collectBoolean(BooleanFunction<? super V> booleanFunction);
+    MutableBooleanBag collectBoolean(BooleanFunction<? super V> booleanFunction);
 
-    MutableByteCollection collectByte(ByteFunction<? super V> byteFunction);
+    MutableByteBag collectByte(ByteFunction<? super V> byteFunction);
 
-    MutableCharCollection collectChar(CharFunction<? super V> charFunction);
+    MutableCharBag collectChar(CharFunction<? super V> charFunction);
 
-    MutableDoubleCollection collectDouble(DoubleFunction<? super V> doubleFunction);
+    MutableDoubleBag collectDouble(DoubleFunction<? super V> doubleFunction);
 
-    MutableFloatCollection collectFloat(FloatFunction<? super V> floatFunction);
+    MutableFloatBag collectFloat(FloatFunction<? super V> floatFunction);
 
-    MutableIntCollection collectInt(IntFunction<? super V> intFunction);
+    MutableIntBag collectInt(IntFunction<? super V> intFunction);
 
-    MutableLongCollection collectLong(LongFunction<? super V> longFunction);
+    MutableLongBag collectLong(LongFunction<? super V> longFunction);
 
-    MutableShortCollection collectShort(ShortFunction<? super V> shortFunction);
+    MutableShortBag collectShort(ShortFunction<? super V> shortFunction);
 
-    <P, VV> MutableCollection<VV> collectWith(Function2<? super V, ? super P, ? extends VV> function, P parameter);
+    <P, VV> MutableBag<VV> collectWith(Function2<? super V, ? super P, ? extends VV> function, P parameter);
+
+    <VV> MutableBag<VV> flatCollect(Function<? super V, ? extends Iterable<VV>> function);
 
     <S> MutableBag<S> selectInstancesOf(Class<S> clazz);
 
-    MutableCollection<V> select(Predicate<? super V> predicate);
+    MutableBag<V> select(Predicate<? super V> predicate);
 
-    <P> MutableCollection<V> selectWith(Predicate2<? super V, ? super P> predicate, P parameter);
+    <P> MutableBag<V> selectWith(Predicate2<? super V, ? super P> predicate, P parameter);
 
-    MutableCollection<V> reject(Predicate<? super V> predicate);
+    MutableBag<V> reject(Predicate<? super V> predicate);
 
-    <P> MutableCollection<V> rejectWith(Predicate2<? super V, ? super P> predicate, P parameter);
+    <P> MutableBag<V> rejectWith(Predicate2<? super V, ? super P> predicate, P parameter);
 
     PartitionMutableBag<V> partition(Predicate<? super V> predicate);
 
---
+++ eclipse-collections-api/src/main/java/org/eclipse/collections/api/map/primitive/PrimitiveObjectMap.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2016 Goldman Sachs.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.api.map.primitive;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.bag.Bag;
+import org.eclipse.collections.api.bag.primitive.BooleanBag;
+import org.eclipse.collections.api.bag.primitive.ByteBag;
+import org.eclipse.collections.api.bag.primitive.CharBag;
+import org.eclipse.collections.api.bag.primitive.DoubleBag;
+import org.eclipse.collections.api.bag.primitive.FloatBag;
+import org.eclipse.collections.api.bag.primitive.IntBag;
+import org.eclipse.collections.api.bag.primitive.LongBag;
+import org.eclipse.collections.api.bag.primitive.ShortBag;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.api.block.function.Function2;
+import org.eclipse.collections.api.block.function.primitive.BooleanFunction;
+import org.eclipse.collections.api.block.function.primitive.ByteFunction;
+import org.eclipse.collections.api.block.function.primitive.CharFunction;
+import org.eclipse.collections.api.block.function.primitive.DoubleFunction;
+import org.eclipse.collections.api.block.function.primitive.FloatFunction;
+import org.eclipse.collections.api.block.function.primitive.IntFunction;
+import org.eclipse.collections.api.block.function.primitive.LongFunction;
+import org.eclipse.collections.api.block.function.primitive.ShortFunction;
+import org.eclipse.collections.api.block.predicate.Predicate;
+import org.eclipse.collections.api.block.predicate.Predicate2;
+import org.eclipse.collections.api.block.procedure.Procedure;
+import org.eclipse.collections.api.map.UnsortedMapIterable;
+import org.eclipse.collections.api.multimap.bag.BagMultimap;
+import org.eclipse.collections.api.ordered.OrderedIterable;
+import org.eclipse.collections.api.partition.bag.PartitionBag;
+import org.eclipse.collections.api.set.UnsortedSetIterable;
+import org.eclipse.collections.api.tuple.Pair;
+
+/**
+ * @since 8.0.
+ */
+public interface PrimitiveObjectMap<V> extends RichIterable<V>
+{
+    boolean containsValue(Object value);
+
+    void forEachValue(Procedure<? super V> procedure);
+
+    /**
+     * Follows the same general contract as {@link Map#equals(Object)}.
+     */
+    @Override
+    boolean equals(Object o);
+
+    /**
+     * Follows the same general contract as {@link Map#hashCode()}.
+     */
+    @Override
+    int hashCode();
+
+    /**
+     * Returns a string representation of this PrimitiveObjectMap. The string representation consists of a list of the
+     * map's key-value pairs in the order they are returned by its iterator. The key and value in each key-value pair are separated
+     * by a colon (<tt>":"</tt>) and each pair is enclosed in square brackets (<tt>"[]"</tt>). Adjacent key-value pairs
+     * are separated by the characters <tt>", "</tt> (comma and space). Keys and values are converted to strings as by
+     * String#valueOf().
+     *
+     * @return a string representation of this PrimitiveObjectMap
+     */
+    String toString();
+
+    Collection<V> values();
+
+    Bag<V> select(Predicate<? super V> predicate);
+
+    <P> Bag<V> selectWith(Predicate2<? super V, ? super P> predicate, P parameter);
+
+    Bag<V> reject(Predicate<? super V> predicate);
+
+    <P> Bag<V> rejectWith(Predicate2<? super V, ? super P> predicate, P parameter);
+
+    PartitionBag<V> partition(Predicate<? super V> predicate);
+
+    <P> PartitionBag<V> partitionWith(Predicate2<? super V, ? super P> predicate, P parameter);
+
+    <S> Bag<S> selectInstancesOf(Class<S> clazz);
+
+    <VV> Bag<VV> collect(Function<? super V, ? extends VV> function);
+
+    BooleanBag collectBoolean(BooleanFunction<? super V> booleanFunction);
+
+    ByteBag collectByte(ByteFunction<? super V> byteFunction);
+
+    CharBag collectChar(CharFunction<? super V> charFunction);
+
+    DoubleBag collectDouble(DoubleFunction<? super V> doubleFunction);
+
+    FloatBag collectFloat(FloatFunction<? super V> floatFunction);
+
+    IntBag collectInt(IntFunction<? super V> intFunction);
+
+    LongBag collectLong(LongFunction<? super V> longFunction);
+
+    ShortBag collectShort(ShortFunction<? super V> shortFunction);
+
+    <P, VV> Bag<VV> collectWith(Function2<? super V, ? super P, ? extends VV> function, P parameter);
+
+    <VV> Bag<VV> collectIf(Predicate<? super V> predicate, Function<? super V, ? extends VV> function);
+
+    <VV> Bag<VV> flatCollect(Function<? super V, ? extends Iterable<VV>> function);
+
+    <VV> BagMultimap<VV, V> groupBy(Function<? super V, ? extends VV> function);
+
+    <VV> BagMultimap<VV, V> groupByEach(Function<? super V, ? extends Iterable<VV>> function);
+
+    <VV> UnsortedMapIterable<VV, V> groupByUniqueKey(Function<? super V, ? extends VV> function);
+
+    /**
+     * @deprecated in 6.0. Use {@link OrderedIterable#zip(Iterable)} instead.
+     */
+    @Deprecated
+    <S> Bag<Pair<V, S>> zip(Iterable<S> that);
+
+    /**
+     * @deprecated in 6.0. Use {@link OrderedIterable#zipWithIndex()} instead.
+     */
+    @Deprecated
+    UnsortedSetIterable<Pair<V, Integer>> zipWithIndex();
+}
