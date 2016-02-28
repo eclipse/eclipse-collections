826a0078691fcc8f806c5710da0fab1eee0e2e44
Craig P. Motlin <craig.motlin@gs.com>
2016-02-28 12:46:24 -0500

    Optimize DropIterable to not delegate to an iterator.

---
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/lazy/DropIterableTestNoIteratorTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Goldman Sachs.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.test.lazy;
+
+import org.eclipse.collections.api.LazyIterable;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.lazy.DropIterable;
+import org.eclipse.collections.test.IterableTestCase;
+import org.eclipse.collections.test.LazyNoIteratorTestCase;
+import org.eclipse.collections.test.list.mutable.FastListNoIterator;
+import org.eclipse.junit.runners.Java8Runner;
+import org.junit.runner.RunWith;
+
+@RunWith(Java8Runner.class)
+public class DropIterableTestNoIteratorTest implements LazyNoIteratorTestCase
+{
+    @Override
+    public <T> LazyIterable<T> newWith(T... elements)
+    {
+        MutableList<T> list = new FastListNoIterator<>();
+        IterableTestCase.addAllTo(elements, list);
+        return new DropIterable<>(list, 0);
+    }
+}
