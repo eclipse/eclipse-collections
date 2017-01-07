0752bd8e8f710c900b0e185507b2b00cb67438e3
Donald Raab <Donald.Raab@gs.com>
2017-01-06 20:39:38 -0500

    Optimize and increase test coverage for CaseFunction and CaseProcedure.
    Signed-off-by: Donald Raab <Donald.Raab@gs.com>

--- unit-tests/src/test/java/org/eclipse/collections/impl/block/procedure/CaseProcedureTest.java
+++ unit-tests/src/test/java/org/eclipse/collections/impl/block/procedure/CaseProcedureTest.java
@@ -10,9 +10,13 @@
 
 package org.eclipse.collections.impl.block.procedure;
 
+import java.util.List;
+
 import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.block.factory.Predicates;
 import org.eclipse.collections.impl.factory.Lists;
 import org.eclipse.collections.impl.list.mutable.FastList;
+import org.eclipse.collections.impl.test.Verify;
 import org.eclipse.collections.impl.utility.Iterate;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,13 +24,65 @@ import org.junit.Test;
 public class CaseProcedureTest
 {
     @Test
-    public void procedure()
+    public void noopCaseAndThenDefault()
+    {
+        List<String> result = Lists.mutable.empty();
+        FastList<String> strings = FastList.newListWith("1", "2");
+        CaseProcedure<String> procedure = new CaseProcedure<>();
+        strings.each(procedure);
+        Verify.assertEmpty(result);
+        procedure.setDefault(result::add);
+        strings.each(procedure);
+        Assert.assertEquals(result, strings);
+        Verify.assertContains("CaseProcedure", procedure.toString());
+    }
+
+    @Test
+    public void oneCaseWithDefault()
     {
         MutableList<String> ifOneList = Lists.mutable.of();
         MutableList<String> defaultList = Lists.mutable.of();
         MutableList<String> list = FastList.newListWith("1", "2");
-        Iterate.forEach(list, new CaseProcedure<String>(defaultList::add).addCase("1"::equals, ifOneList::add));
+        CaseProcedure<String> procedure =
+                new CaseProcedure<String>(defaultList::add)
+                        .addCase("1"::equals, ifOneList::add);
+        list.each(procedure);
         Assert.assertEquals(FastList.newListWith("1"), ifOneList);
         Assert.assertEquals(FastList.newListWith("2"), defaultList);
+        Verify.assertContains("CaseProcedure", procedure.toString());
+    }
+
+    @Test
+    public void twoCasesNoDefault()
+    {
+        MutableList<String> ifOneList = Lists.mutable.of();
+        MutableList<String> ifTwoList = Lists.mutable.of();
+        MutableList<String> list = FastList.newListWith("1", "2", "3");
+        CaseProcedure<String> procedure =
+                new CaseProcedure<String>()
+                        .addCase("1"::equals, ifOneList::add)
+                        .addCase("2"::equals, ifTwoList::add);
+        list.each(procedure);
+        Assert.assertEquals(FastList.newListWith("1"), ifOneList);
+        Assert.assertEquals(FastList.newListWith("2"), ifTwoList);
+        Verify.assertContains("CaseProcedure", procedure.toString());
+    }
+
+    @Test
+    public void twoCasesWithDefault()
+    {
+        MutableList<String> ifOneList = Lists.mutable.of();
+        MutableList<String> ifTwoList = Lists.mutable.of();
+        MutableList<String> defaultList = Lists.mutable.of();
+        MutableList<String> list = FastList.newListWith("1", "2", "3", "4");
+        CaseProcedure<String> procedure =
+                new CaseProcedure<String>(defaultList::add)
+                        .addCase("1"::equals, ifOneList::add)
+                        .addCase("2"::equals, ifTwoList::add);
+        list.each(procedure);
+        Assert.assertEquals(FastList.newListWith("1"), ifOneList);
+        Assert.assertEquals(FastList.newListWith("2"), ifTwoList);
+        Assert.assertEquals(FastList.newListWith("3", "4"), defaultList);
+        Verify.assertContains("CaseProcedure", procedure.toString());
     }
 }
