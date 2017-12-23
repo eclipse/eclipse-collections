59b67a348db8f2ccb19d1268679d438f4abde8af
Ivan Sopov <sopov.ivan@gmail.com>
2017-12-23 21:49:34 +0300

    add jcstress-based tests for concurrent collections
    Signed-off-by: Ivan Sopov <sopov.ivan@gmail.com>

---
+++ jcstress-tests/src/main/java/org/eclipse/collections/impl/map/mutable/ConcurrentHashMapUnsafeValuesRemoveIfTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2018 Ivan Sopov and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.eclipse.collections.impl.map.mutable;
+
+import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.Arbiter;
+import org.openjdk.jcstress.annotations.Expect;
+import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.State;
+import org.openjdk.jcstress.infra.results.Z_Result;
+
+@JCStressTest
+@Outcome(id = "false", expect = Expect.ACCEPTABLE)
+@Outcome(expect = Expect.FORBIDDEN)
+@State
+public class ConcurrentHashMapUnsafeValuesRemoveIfTest
+{
+    private final ConcurrentHashMapUnsafe<Integer, Boolean> map = new ConcurrentHashMapUnsafe<>();
+
+    @Actor
+    public void setFoo()
+    {
+        map.put(1, false);
+    }
+
+    @Actor
+    public void removeIf()
+    {
+        map.put(1, true);
+        map.values().removeIf(Boolean::valueOf);
+    }
+
+    @Arbiter
+    public void after(Z_Result r)
+    {
+        r.r1 = map.containsKey(1) && map.get(1);
+    }
+}
