6fdd2416d34bb92b3b7e554cb3b9f9fe6cca6d7e
Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>
2021-03-17 22:55:58 +0100

    Update copyright headers to 2021.
    Signed-off-by: Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>

--- unit-tests-java8/src/test/java/org/eclipse/collections/test/map/mutable/UnifiedMapWithHashingStrategyNoIteratorTest.java
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/map/mutable/UnifiedMapWithHashingStrategyNoIteratorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Hiroshi Ito.
+ * Copyright (c) 2021 Hiroshi Ito.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
