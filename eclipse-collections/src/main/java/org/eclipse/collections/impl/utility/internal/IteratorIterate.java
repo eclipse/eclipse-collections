0f1b853df93845df20e8c97f447ed6bb013da6b5
Donald Raab <Donald.Raab@bnymellon.com>
2022-03-06 18:44:59 -0500

    Remove dependencies on primitive collection implementations.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/utility/internal/IteratorIterate.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/utility/internal/IteratorIterate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Goldman Sachs.
+ * Copyright (c) 2022 Goldman Sachs and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
@@ -52,6 +52,14 @@ import org.eclipse.collections.api.collection.primitive.MutableIntCollection;
 import org.eclipse.collections.api.collection.primitive.MutableLongCollection;
 import org.eclipse.collections.api.collection.primitive.MutableShortCollection;
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.primitive.BooleanLists;
+import org.eclipse.collections.api.factory.primitive.ByteLists;
+import org.eclipse.collections.api.factory.primitive.CharLists;
+import org.eclipse.collections.api.factory.primitive.DoubleLists;
+import org.eclipse.collections.api.factory.primitive.FloatLists;
+import org.eclipse.collections.api.factory.primitive.IntLists;
+import org.eclipse.collections.api.factory.primitive.LongLists;
+import org.eclipse.collections.api.factory.primitive.ShortLists;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.map.MutableMap;
 import org.eclipse.collections.api.map.MutableMapIterable;
@@ -65,14 +73,6 @@ import org.eclipse.collections.impl.block.factory.Functions0;
 import org.eclipse.collections.impl.block.procedure.MutatingAggregationProcedure;
 import org.eclipse.collections.impl.block.procedure.NonMutatingAggregationProcedure;
 import org.eclipse.collections.impl.list.mutable.FastList;
-import org.eclipse.collections.impl.list.mutable.primitive.BooleanArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.ByteArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.CharArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.DoubleArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.FloatArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.IntArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.LongArrayList;
-import org.eclipse.collections.impl.list.mutable.primitive.ShortArrayList;
 import org.eclipse.collections.impl.map.mutable.UnifiedMap;
 import org.eclipse.collections.impl.multimap.list.FastListMultimap;
 import org.eclipse.collections.impl.partition.list.PartitionFastList;
@@ -385,7 +385,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             BooleanFunction<? super T> booleanFunction)
     {
-        MutableBooleanCollection result = new BooleanArrayList();
+        MutableBooleanCollection result = BooleanLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(booleanFunction.booleanValueOf(iterator.next()));
@@ -415,7 +415,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             ByteFunction<? super T> byteFunction)
     {
-        MutableByteCollection result = new ByteArrayList();
+        MutableByteCollection result = ByteLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(byteFunction.byteValueOf(iterator.next()));
@@ -445,7 +445,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             CharFunction<? super T> charFunction)
     {
-        MutableCharCollection result = new CharArrayList();
+        MutableCharCollection result = CharLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(charFunction.charValueOf(iterator.next()));
@@ -475,7 +475,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             DoubleFunction<? super T> doubleFunction)
     {
-        MutableDoubleCollection result = new DoubleArrayList();
+        MutableDoubleCollection result = DoubleLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(doubleFunction.doubleValueOf(iterator.next()));
@@ -505,7 +505,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             FloatFunction<? super T> floatFunction)
     {
-        MutableFloatCollection result = new FloatArrayList();
+        MutableFloatCollection result = FloatLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(floatFunction.floatValueOf(iterator.next()));
@@ -535,7 +535,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             IntFunction<? super T> intFunction)
     {
-        MutableIntCollection result = new IntArrayList();
+        MutableIntCollection result = IntLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(intFunction.intValueOf(iterator.next()));
@@ -565,7 +565,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             LongFunction<? super T> longFunction)
     {
-        MutableLongCollection result = new LongArrayList();
+        MutableLongCollection result = LongLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(longFunction.longValueOf(iterator.next()));
@@ -595,7 +595,7 @@ public final class IteratorIterate
             Iterator<T> iterator,
             ShortFunction<? super T> shortFunction)
     {
-        MutableShortCollection result = new ShortArrayList();
+        MutableShortCollection result = ShortLists.mutable.empty();
         while (iterator.hasNext())
         {
             result.add(shortFunction.shortValueOf(iterator.next()));
