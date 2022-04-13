219dfa29c2b287a048d1b8409acd3e7c404adad7
Matthew Dietz <Matthew.Dietz@bnymellon.com>
2022-04-13 16:27:46 -0400

    Add package.info to org.eclipse.collections.api.annotation. closes #1246
    Signed-off-by: Matthew Dietz <Matthew.Dietz@bnymellon.com>

---
+++ eclipse-collections-api/src/main/java/org/eclipse/collections/api/annotation/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Goldman Sachs and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompany this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+/**
+ * This package contains interfaces for Annotation API.
+ * <p>
+ *     The Annotation package is a supporting package that autogenerates code to build annotations
+ *     needed by the rest of the api package
+ * <p>
+ *     This package contains 1 interface:
+ * <ul>
+ *     <li>
+ *          {@link org.eclipse.collections.api.annotation.Beta} - contains the interface tagged Beta
+ *     </li>
+ * </ul>
+ */
+package org.eclipse.collections.api.annotation;
