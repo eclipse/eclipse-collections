64092858c91ac6b8b1d1f4dca7675a85140e367c
Craig P. Motlin <cmotlin@gmail.com>
2022-04-13 22:21:34 -0400

    Fix test to make assertion about unused variable.

--- unit-tests/src/test/java/org/eclipse/collections/impl/map/mutable/MutableMapIterableTestCase.java
+++ unit-tests/src/test/java/org/eclipse/collections/impl/map/mutable/MutableMapIterableTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Goldman Sachs.
+ * Copyright (c) 2022 Goldman Sachs and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v. 1.0 which accompany this distribution.
@@ -560,7 +560,8 @@ public abstract class MutableMapIterableTestCase extends MapIterableTestCase
         Verify.assertMapsEqual(UnifiedMap.newWithKeysValues("A", 1, "B", 2), mapWith);
 
         MutableMapIterable<String, Integer> mapWith2 = mapWith.withKeyValue("A", 11);
-        Verify.assertMapsEqual(UnifiedMap.newWithKeysValues("A", 11, "B", 2), mapWith);
+        Verify.assertMapsEqual(UnifiedMap.newWithKeysValues("A", 11, "B", 2), mapWith2);
+        Assert.assertSame(mapWith, mapWith2);
     }
 
     @Test
