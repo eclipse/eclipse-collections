51edf9fca342279083e07446a861587c5120e21f
Craig P. Motlin <cmotlin@gmail.com>
2024-08-06 16:46:43 -0400

    Reduce duplication between tests for unmodifiable maps.

--- unit-tests-java8/src/test/java/org/eclipse/collections/test/map/mutable/ordered/MutableOrderedMapTestCase.java
+++ unit-tests-java8/src/test/java/org/eclipse/collections/test/map/mutable/ordered/MutableOrderedMapTestCase.java
@@ -10,19 +10,10 @@
 
 package org.eclipse.collections.test.map.mutable.ordered;
 
-import org.eclipse.collections.api.factory.Maps;
-import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.map.MutableOrderedMap;
-import org.eclipse.collections.impl.test.Verify;
 import org.eclipse.collections.test.MutableOrderedIterableTestCase;
 import org.eclipse.collections.test.map.OrderedMapIterableTestCase;
 import org.eclipse.collections.test.map.mutable.MutableMapIterableTestCase;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public interface MutableOrderedMapTestCase extends OrderedMapIterableTestCase, MutableMapIterableTestCase, MutableOrderedIterableTestCase
 {
@@ -31,27 +22,4 @@ public interface MutableOrderedMapTestCase extends OrderedMapIterableTestCase, M
 
     @Override
     <K, V> MutableOrderedMap<K, V> newWithKeysValues(Object... elements);
-
-    @Test
-    default void MutableOrderedMap_removeAllKeys()
-    {
-        MutableOrderedMap<Integer, String> map = this.newWithKeysValues(1, "1", 2, "Two", 3, "Three");
-
-        assertThrows(NullPointerException.class, () -> map.removeAllKeys(null));
-        assertFalse(map.removeAllKeys(Sets.mutable.empty()));
-        assertFalse(map.removeAllKeys(Sets.mutable.with(4)));
-        assertFalse(map.removeAllKeys(Sets.mutable.with(4, 5, 6)));
-        assertFalse(map.removeAllKeys(Sets.mutable.with(4, 5, 6, 7, 8, 9)));
-
-        assertTrue(map.removeAllKeys(Sets.mutable.with(1)));
-        Verify.denyContainsKey(1, map);
-        assertTrue(map.removeAllKeys(Sets.mutable.with(3, 4, 5, 6, 7)));
-        Verify.denyContainsKey(3, map);
-
-        map.putAll(Maps.mutable.with(4, "Four", 5, "Five", 6, "Six", 7, "Seven"));
-        assertTrue(map.removeAllKeys(Sets.mutable.with(2, 3, 9, 10)));
-        Verify.denyContainsKey(2, map);
-        assertTrue(map.removeAllKeys(Sets.mutable.with(5, 3, 7, 8, 9)));
-        assertEquals(this.newWithKeysValues(4, "Four", 6, "Six"), map);
-    }
 }
